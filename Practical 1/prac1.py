c3f86fe0b8f70f1db9eb28dd911a0d22725530ef
Raj Makwana <124243367+raj0501@users.noreply.github.com>
2024-02-09 19:01:53 +0530

    Add files via upload

---
+++ Practical 1/prac1.py
@@ -0,0 +1,9 @@
+# D23CE174 Raj Makwana
+
+# B) Write a python program to calculate simple interest.
+
+p=int(input("Enter P : "))
+t=int(input("Enter T : "))
+i=int(input("Enter i : "))
+intrest = float((p*t*i)/100)
+print('Intrest = ',intrest)
